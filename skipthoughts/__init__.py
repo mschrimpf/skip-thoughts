543806c7fbede04cc518fab616e1553e6db1f1c9
Martin Schrimpf <m4rtinsch@gmail.com>
2020-01-05 12:06:21 -0500

    pass `allow_pickle=True` when loading tables to work with newest numpy

--- skipthoughts/__init__.py
+++ skipthoughts/__init__.py
@@ -69,8 +69,8 @@ def load_tables(path_to_tables='models/'):
     Load the tables
     """
     words = []
-    utable = numpy.load(path_to_tables + 'utable.npy', encoding='latin1')
-    btable = numpy.load(path_to_tables + 'btable.npy', encoding='latin1')
+    utable = numpy.load(path_to_tables + 'utable.npy', encoding='latin1', allow_pickle=True)
+    btable = numpy.load(path_to_tables + 'btable.npy', encoding='latin1', allow_pickle=True)
     f = open(path_to_tables + 'dictionary.txt', 'rb')
     for line in f:
         words.append(line.decode('utf-8').strip())
